db92c186be3898a941bdf80df8891d8a621c8273
Gabriel <gabriel.bejarano@digitalcomtech.com>
2017-08-02 12:33:00 -0500

    code-push added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,58 +4,60 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 import { Api } from "../providers/api";
 import { LoginPage } from "../pages/login/login";
 import { TicketsPage } from "../pages/tickets/tickets";
 
+import { CodePush } from '@ionic-native/code-push';
+
 @Component({
-  templateUrl: 'app.html'
+	templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+	@ViewChild(Nav) nav: Nav;
 
-  rootPage;
+	rootPage;
 
-  pages: Array<{ title: string, component: any, icon: string }>;
+	pages: Array<{ title: string, component: any, icon: string }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public api: Api) {
-    this.initializeApp();
+	constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public api: Api, private codePush: CodePush) {
+		this.initializeApp();
 
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage, icon: 'home' },
-      { title: 'Tickets', component: TicketsPage, icon: 'list' }
-    ];
+		// used for an example of ngFor and navigation
+		this.pages = [
+			{ title: 'Home', component: HomePage, icon: 'home' },
+			{ title: 'Tickets', component: TicketsPage, icon: 'list' }
+		];
 
-  }
+	}
 
-  initializeApp() {
-    this.api.ready.then(() => {
-      if (this.api.user)
-        this.rootPage = HomePage
-      else
-        this.rootPage = LoginPage
-    });
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+	initializeApp() {
+		this.api.ready.then(() => {
+			if (this.api.user)
+				this.rootPage = HomePage
+			else
+				this.rootPage = LoginPage
+		});
+		this.platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			this.statusBar.styleDefault();
+			this.splashScreen.hide();
+			this.codePush.sync().subscribe((syncStatus) => console.log(syncStatus), (err) => { console.warn(err) });
+		});
+	}
 
-  logout() {
-    this.api.storage.clear();
-    this.rootPage = LoginPage;
-    setTimeout(() => {
-      this.api.user = undefined;
-    }, 1000)
-  }
+	logout() {
+		this.api.storage.clear();
+		this.rootPage = LoginPage;
+		setTimeout(() => {
+			this.api.user = undefined;
+		}, 1000)
+	}
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+	openPage(page) {
+		// Reset the content nav to have just this page
+		// we wouldn't want the back button to show in this scenario
+		this.nav.setRoot(page.component);
+	}
 }
