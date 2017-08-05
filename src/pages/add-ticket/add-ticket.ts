08938a6d26e454b4bfbcafe68f417723b601cfc2
seedgabo <seedgabo@gmail.com>
2017-08-05 01:43:35 -0500

    loader and guardian

--- src/pages/add-ticket/add-ticket.ts
+++ src/pages/add-ticket/add-ticket.ts
@@ -3,69 +3,73 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Api } from "../../providers/api";
 
 @Component({
-	selector: 'page-add-ticket',
-	templateUrl: 'add-ticket.html',
+  selector: 'page-add-ticket',
+  templateUrl: 'add-ticket.html',
 })
 export class AddTicketPage {
-	ticket;
-	image;
-	constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api) {
-		this.ticket = navParams.get('ticket');
-		if (!this.ticket)
-			this.ticket = { titulo: '', contenido: '', user_id: this.api.user.id, categoria_id: 1, estado: 'abierto' }
-	}
+  ticket;
+  image;
+  loading = false;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api) {
+    this.ticket = navParams.get('ticket');
+    if (!this.ticket)
+      this.ticket = { titulo: '', contenido: '', user_id: this.api.user.id, categoria_id: 1, estado: 'abierto', guardian_id: 6 }
+  }
 
-	ionViewDidLoad() {
-		if (this.api.categorias && this.api.categorias.length > 0)
-			this.ticket.categoria_id = this.api.categorias[0].id;
-	}
-	save() {
-		if (this.ticket.id) {
-			this.api.put('tickets/' + this.ticket.id, this.ticket)
-				.then((data: any) => {
-					this.ticket = data;
-					if (this.image) {
-						this.uploadImage(data.id);
-					}
-					this.navCtrl.pop();
-				})
-				.catch(console.error)
-		} else {
-			this.api.post('tickets', this.ticket)
-				.then((data: any) => {
-					this.ticket = data;
-					if (this.image) {
-						this.uploadImage(data.id);
-					}
-					this.navCtrl.pop();
-				})
-				.catch(console.error)
-		}
-	}
+  ionViewDidLoad() {
+    if (this.api.categorias && this.api.categorias.length > 0)
+      this.ticket.categoria_id = this.api.categorias[0].id;
+  }
+  save() {
+    this.loading = true;
+    if (this.ticket.id) {
+      this.api.put('tickets/' + this.ticket.id, this.ticket)
+        .then((data: any) => {
+          this.ticket = data;
+          this.loading = false;
+          if (this.image) {
+            this.uploadImage(data.id);
+          }
+          this.navCtrl.pop();
+        })
+        .catch(console.error)
+    } else {
+      this.api.post('tickets', this.ticket)
+        .then((data: any) => {
+          this.ticket = data;
+          this.loading = false;
+          if (this.image) {
+            this.uploadImage(data.id);
+          }
+          this.navCtrl.pop();
+        })
+        .catch(console.error)
+    }
+  }
 
-	askFile() {
-		var filer: any = document.querySelector("#input-file")
-		filer.click();
-	}
-	verFile(event) {
-		var reader: any = new FileReader();
-		reader.readAsDataURL(event.target.files[0])
-		reader.onload = (result) => {
-			this.image = result.target.result;
-		};
-	}
+  askFile() {
+    var filer: any = document.querySelector("#input-file")
+    filer.click();
+  }
+  verFile(event) {
+    var reader: any = new FileReader();
+    reader.readAsDataURL(event.target.files[0])
+    reader.onload = (result) => {
+      this.image = result.target.result;
+    };
+  }
 
-	uploadImage(id) {
-		this.api.post('images/upload/tickets/' + id, { image: this.image })
-			.then((data) => {
-				console.log(data);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}
+  uploadImage(id) {
+    this.api.post('images/upload/tickets/' + id, { image: this.image })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
-	canSave() {
-		return this.ticket.titulo.length > 3 && this.ticket.categoria_id > 0;
-	}
+  canSave() {
+    return this.ticket.titulo.length > 3 && this.ticket.categoria_id > 0;
+  }
 }
